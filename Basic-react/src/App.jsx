3cdb01b9a2bb3a7d34ac1e14aae94ede276b6344
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-02-04 23:25:24 +0530

    feat:add some basic of react (render array on ui)

--- Basic-react/src/App.jsx
+++ Basic-react/src/App.jsx
@@ -1,24 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './Navbar'
-import Item from'./components/Item'
-import ItemDate from './components/ItemDate'
-function App() {
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Nav from './Navbar'
+// import Item from'./components/Item'
+// import ItemDate from './components/ItemDate'
+// function App() {
   
-  return (
-    <>
-      <div>
-        <h1>Hello React</h1>
-        <Nav/> 
-      </div>
-    <Item/>
+//   return (
+//     <>
+//       <div>
+//         <h1>Hello React</h1>
+//         <Nav/> 
+//       </div>
+//     <Item/>
     
     
      
+//     </>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [isLoggedIn,setLoggedIn]=  useState(false)
+//   return (
+//     <>
+//   {!isLoggedIn}
+//    ;</>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  const [items,setItems]=useState(["Banana","graps","mango"]);
+  return (
+    <>
+      <ul>
+        {items.map((item,index)=> (<li key={index}>
+        {item}
+        </li>))}
+      </ul>
     </>
   )
 }
 
 export default App
+
+
