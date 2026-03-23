b40bb9b0ff2f8adc66a47f950f0bf9b724663653
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-03-23 23:49:55 +0530

    feat: add form handeling and two way binding concept

--- Basic-react/src/App.jsx
+++ Basic-react/src/App.jsx
@@ -6,17 +6,14 @@
 // import Item from'./components/Item'
 // import ItemDate from './components/ItemDate'
 // function App() {
-  
+
 //   return (
 //     <>
 //       <div>
 //         <h1>Hello React</h1>
-//         <Nav/> 
+//         <Nav/>
 //       </div>
 //     <Item/>
-    
-    
-     
 //     </>
 //   )
 // }
@@ -35,22 +32,58 @@
 // }
 
 // export default App
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
+// import Data from './hooks/Data';
 
-function App() {
-  const [items,setItems]=useState(["Banana","graps","mango"]);
-  return (
-    <>
-      <ul>
+// function App() {
+//   const [items,setItems]=useState(["Banana","graps","mango"]);
+//   return (
+//     <>
+      {/* <ul>
         {items.map((item,index)=> (<li key={index}>
         {item}
         </li>))}
-      </ul>
-    </>
+      </ul> */}
+      // <Data/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import React from 'react'
+
+const age = 18;
+
+const App = () => {
+  let arr = ["spiderMan", "ironMan", "thor", "Wanda", "black Panther"];
+
+  return (
+
+    <div>
+      {/* <h1 style={{
+  maxWidth:"1240px",
+  margin:"0 auto",
+  width:"100%",
+  padding:"2rem",
+  textAlign:"center",
+  backgroundColor:"yellow"
+}}>Hello inline CSS</h1> */}
+      {/* Conditional Rendering */}
+      {age > 18 ? <h1>Adult</h1> : <h1>Child</h1>}
+
+      {/* List Rendering */}
+      {arr.map((item, index) => (
+        <h1 key={index}>{item}</h1>
+      ))}
+    </div>
   )
 }
 
 export default App
+
 
 
